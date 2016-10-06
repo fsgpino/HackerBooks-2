96ba0842344ae8013b3078135860c75a596e3690
Francisco Solano Gómez Pino <fsgpino@gmail.com>
2016-10-06 18:19:32 +0200

    fix typo

--- HackerBooks 2/AppDelegate.swift
+++ HackerBooks 2/AppDelegate.swift
@@ -122,7 +122,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 		
-		print("¡Good bye! ¡May the Force be with you!")
+		print("Good bye! May the Force be with you!")
     }
 
 
